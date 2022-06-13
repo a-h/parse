326853697e2e05762f4b831d4c3b623b9c03b9e4
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-06-13 17:57:05 +0100

    feat: add until parser

---
+++ until_test.go
@@ -0,0 +1,40 @@
+package parse_test
+
+import (
+	"testing"
+
+	"github.com/a-h/parse"
+)
+
+func TestUntil(t *testing.T) {
+	tests := []ParserTest[[]string]{
+		{
+			name:          "Until: success",
+			input:         "ABCDEF",
+			parser:        parse.Until(parse.AnyRune, parse.String("D")),
+			expectedMatch: []string{"A", "B", "C"},
+			expectedOK:    true,
+		},
+		{
+			name:       "Until: fail, reached EOF before delimiter was found",
+			input:      "ABCDEF",
+			parser:     parse.Until(parse.AnyRune, parse.String("G")),
+			expectedOK: false,
+		},
+		{
+			name:          "UntilEOF: stop at the delimiter if it's there",
+			input:         "ABCDEF",
+			parser:        parse.UntilEOF(parse.AnyRune, parse.String("F")),
+			expectedMatch: []string{"A", "B", "C", "D", "E"},
+			expectedOK:    true,
+		},
+		{
+			name:          "UntilEOF: allow EOF",
+			input:         "ABCDEF",
+			parser:        parse.UntilEOF(parse.AnyRune, parse.String("G")),
+			expectedMatch: []string{"A", "B", "C", "D", "E", "F"},
+			expectedOK:    true,
+		},
+	}
+	RunParserTests(t, tests)
+}
